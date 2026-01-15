d5b4a88a238548b2193746934090e49b6e2e9f9b
matias_estigarribia <matiestiga@gmail.com>
2026-01-15 13:59:14 -0300

    fix(command): change extension list to tuple

--- photos/management/commands/import_photos.py
+++ photos/management/commands/import_photos.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
 from django.core.files import File
 from photos.models import Photo
 from editions.models import Edition
